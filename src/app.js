1fb81bae0ecda0d3467fe0baaf04d48c865b9e5f
soydeif <daed_diaz@hotmail.com>
2021-06-06 15:08:11 +0000

    exercise completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  let randomNumber = Math.random() * 10;
+};
+
+let generateExcuse = () => {
+  let pronoum = ["A", "The"];
+  let subjet = ["dog", "thief", "racoon"];
+  let action = ["bit my", "threw my", "ate my"];
+  let possetion = ["homework", "laptop", "lunch"];
+  let where = ["on the street", "in the pool", "In the zoo"];
+
+  let proIndx = Math.floor(Math.random() * pronoum.length);
+  let subjIndx = Math.floor(Math.random() * subjet.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let possIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoum[proIndx] +
+    " " +
+    subjet[subjIndx] +
+    " " +
+    action[actIndx] +
+    " " +
+    possetion[possIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
